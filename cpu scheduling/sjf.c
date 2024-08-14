dbddba972f1d40a342239d8169560ea7bbc6da49
MEGHA <121880794+MEGHALINN@users.noreply.github.com>
2024-08-14 15:49:43 +0530

    Update sjf.c

--- cpu scheduling/sjf.c
+++ cpu scheduling/sjf.c
@@ -1,60 +1,86 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 struct sjf {
-  int pid;
-  int btime;
-  int atime;  // Added arrival time
-  int wtime;
-  int ttime;
+    int pid;
+    int btime;
+    int atime;
+    int wtime;
+    int ttime;
 };
 
-void sort_by_arrival_and_burst(struct sjf *p, int n) {
-  struct sjf temp;
-  int i, j;
-  for (i = 0; i < n - 1; i++) {
-    for (j = 0; j < n - i - 1; j++) {
-      if (p[j].atime > p[j + 1].atime ||
-          (p[j].atime == p[j + 1].atime && p[j].btime > p[j + 1].btime)) {
-        temp = p[j];
-        p[j] = p[j + 1];
-        p[j + 1] = temp;
-      }
+void sortProcesses(struct sjf p[], int n) {
+    struct sjf temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (p[j].atime > p[j + 1].atime) {
+                temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
     }
-  }
 }
 
 int main() {
-  int i, j, n, completed = 0, current_time = 0;
-  struct sjf temp;
-  float totwt = 0, tottat = 0;
+    int i, j, n, completed = 0, current_time = 0;
+    float totwt = 0, tottat = 0;
 
-  printf("Enter the no of processes :");
-  scanf("%d", &n);
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
 
-  struct sjf p[10];
-  for (i = 0; i < n; i++) {
-    printf("Enter Pid, Burst time and Arrival time for Process %d:", i + 1);
-    scanf("%d %d %d", &p[i].pid, &p[i].btime, &p[i].atime);
-  }
-
-  sort_by_arrival_and_burst(p, n);  // Sort by arrival time (primary) and burst time (secondary)
-
-  printf("Pid\tBurst Time\tArrival Time\tWaiting Time\tTurnaround Time\n");
-  for (i = 0; completed < n; i++) {
-    if (p[i].atime <= current_time) {
-      p[i].wtime = current_time - p[i].atime;
-      p[i].ttime = p[i].wtime + p[i].btime;
-      totwt += p[i].wtime;
-      tottat += p[i].ttime;
-      current_time += p[i].btime;
-      printf("P%d\t%d\t%d\t%d\t%d\n", p[i].pid, p[i].btime, p[i].atime, p[i].wtime, p[i].ttime);
-      completed++;
+    struct sjf p[n];
+    for (i = 0; i < n; i++) {
+        printf("Enter Pid, Burst time, and Arrival time for Process %d: ", i + 1);
+        scanf("%d %d %d", &p[i].pid, &p[i].btime, &p[i].atime);
     }
-  }
 
-  printf("Average Waiting Time: %.2f\n", totwt / n);
-  printf("Average Turnaround Time: %.2f\n", tottat / n);
 
-  return 0;
+    sortProcesses(p, n);
+
+    printf("Pid\tBurst Time\tArrival Time\tWaiting Time\tTurnaround Time\n");
+
+    while (completed < n) {
+        int min_btime_idx = -1;
+
+      
+        for (i = 0; i < n; i++) {
+            if (p[i].atime <= current_time && p[i].btime > 0) {
+                if (min_btime_idx == -1 || p[i].btime < p[min_btime_idx].btime) {
+                    min_btime_idx = i;
+                }
+            }
+        }
+
+        if (min_btime_idx != -1) {
+     
+            p[min_btime_idx].wtime = current_time - p[min_btime_idx].atime;
+            p[min_btime_idx].ttime = p[min_btime_idx].wtime + p[min_btime_idx].btime;
+
+          
+            totwt += p[min_btime_idx].wtime;
+            tottat += p[min_btime_idx].ttime;
+
+     
+            printf("P%d\t%d\t\t%d\t\t%d\t\t%d\n", p[min_btime_idx].pid, p[min_btime_idx].btime, p[min_btime_idx].atime, p[min_btime_idx].wtime, p[min_btime_idx].ttime);
+
+         
+            current_time += p[min_btime_idx].btime;
+            p[min_btime_idx].btime = -1; 
+            completed++;
+        } else {
+            
+            int next_arrival = __INT_MAX__;
+            for (i = 0; i < n; i++) {
+                if (p[i].atime < next_arrival && p[i].btime > 0) {
+                    next_arrival = p[i].atime;
+                }
+            }
+            current_time = next_arrival;
+        }
+    }
+
+    printf("Average Waiting Time: %.2f\n", totwt / n);
+    printf("Average Turnaround Time: %.2f\n", tottat / n);
+
+    return 0;
 }
